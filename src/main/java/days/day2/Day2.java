1d9fef5cc7e5743d43cb8884f4e09d062ba789cd
scibor <scibor.on.github@gmail.com>
2024-12-02 16:10:14 +0100

    [Day 2] isReportMonotone

--- src/main/java/days/day2/Day2.java
+++ src/main/java/days/day2/Day2.java
@@ -6,6 +6,7 @@ import problem.Problem;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Day2 implements Problem {
 
@@ -15,9 +16,26 @@ public class Day2 implements Problem {
 
     public static boolean isReportSafe(List<Integer> list) {
         for (int i = 0; i < list.size() - 1; i++) {
-            if (Math.abs(list.get(i) - list.get(i + 1)) > 2) {
+            if (Math.abs(list.get(i) - list.get(i + 1)) > 3) {
                 return false;
             }
+        }
+        return isReportMonotone(list);
+    }
+
+    public static boolean isReportMonotone(List<Integer> list) {
+        boolean increasing = true;
+        if (list.size() < 2) {
+            return true;
+        }
+        if (list.get(0) > list.get(1)) {
+            increasing = false;
+        } else if (Objects.equals(list.get(0), list.get(1))) {
+            return false;
+        }
+        for (int i = 1; i < list.size() - 1; i++) {
+            if (increasing && list.get(i) >= list.get(i + 1) || !increasing && list.get(i) <= list.get(i + 1))
+                return false;
         }
         return true;
     }
--- src/test/java/days/day2/Day2Test.java
+++ src/test/java/days/day2/Day2Test.java
@@ -16,6 +16,20 @@ public class Day2Test {
     Day2 day2;
     String testData;
 
+
+    @DataProvider(name = "isReportMonotone")
+    public static Object[][] lists2() {
+        return new Object[][]{
+                {List.of(1, 2, 3, 4, 5), true},
+                {List.of(1, 2, 3, 4, 4), false},
+                {List.of(1, 2, 3, 4, 3), false},
+                {List.of(7, 6, 4, 3, 1), true},
+                {List.of(7, 6, 4, 3, 3), false},
+                {List.of(7, 6, 4, 3, 5), false},
+                {List.of(), true}
+        };
+    }
+
     @DataProvider(name = "isReportSafe")
     public static Object[][] lists() {
         return new Object[][]{
@@ -23,7 +37,9 @@ public class Day2Test {
                 {List.of(1, 3, 4, 6, 7), true},
                 {List.of(7, 6, 4, 3, 1), true},
                 {List.of(5, 4, 3, 2, 1), true},
-                {List.of(1, 2, 5), false},
+                {List.of(1, 2, 5), true},
+                {List.of(1, 2, 6), false},
+                {List.of(1, 2, 1), false},
                 {List.of(), true}
         };
     }
@@ -32,6 +48,11 @@ public class Day2Test {
     public void setup() throws IOException {
         day2 = new Day2();
         testData = Utils.readAsString(new File("src/main/resources/inputs/day2-test.txt"));
+    }
+
+    @Test(dataProvider = "isReportMonotone")
+    public void isReportMonotone(List<Integer> list, boolean result) {
+        AssertionsForClassTypes.assertThat(Day2.isReportMonotone(list)).isEqualTo(result);
     }
 
     @Test(dataProvider = "isReportSafe")
