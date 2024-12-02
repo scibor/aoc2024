18b3ee4081d38ad119cbfd6bc892430db8e93541
scibor <scibor.on.github@gmail.com>
2024-12-02 15:48:27 +0100

    [Day 2] parametrized isReportSafe

--- src/test/java/days/day2/Day2Test.java
+++ src/test/java/days/day2/Day2Test.java
@@ -2,6 +2,7 @@ package days.day2;
 
 import org.assertj.core.api.AssertionsForClassTypes;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.Utils;
 
@@ -14,6 +15,17 @@ public class Day2Test {
     Day2 day2;
     String testData;
 
+    @DataProvider(name = "isReportSafe")
+    public static Object[][] lists() {
+        return new Object[][]{
+                {List.of(1, 2, 3, 4, 5), true},
+                {List.of(1, 3, 4, 6, 7), true},
+                {List.of(7, 6, 4, 3, 1), true},
+                {List.of(5, 4, 3, 2, 1), true},
+                {List.of(1, 2, 5), false},
+                {List.of(), true}
+        };
+    }
 
     @BeforeClass
     public void setup() throws IOException {
@@ -21,10 +33,8 @@ public class Day2Test {
         testData = Utils.readAsString(new File("src/main/resources/inputs/day2-test.txt"));
     }
 
-    @Test
-    public void isReportSafe() {
-        List<Integer> list = List.of(1, 2, 3, 4, 5);
-        AssertionsForClassTypes.assertThat(day2.isReportSafe(list)).isTrue();
+    @Test(dataProvider = "isReportSafe")
+    public void isReportSafe(List<Integer> list, boolean result) {
+        AssertionsForClassTypes.assertThat(day2.isReportSafe(list)).isEqualTo(result);
     }
-
 }
