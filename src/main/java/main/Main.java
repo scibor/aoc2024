c4accbbf51631883588c9e0ea5a92caa7d487cf4
scibor <scibor.on.github@gmail.com>
2024-12-02 16:10:47 +0100

    [Day 2] working part 1

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,6 @@
 package main;
 
-import days.day1.Day1;
+import days.day2.Day2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import problem.Problem;
@@ -16,8 +16,8 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException {
-        String fileName = "day1.txt";
-        Problem problem = new Day1();
+        String fileName = "day2.txt";
+        Problem problem = new Day2();
         File inputFile = Path.of("src/main/resources/inputs/" + fileName).toFile();
         String input = Utils.readAsString(inputFile);
         problem.parseInput(input);
